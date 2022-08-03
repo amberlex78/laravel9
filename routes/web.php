e632a805b9e45557f3f7ff8fe81d21d6cfdbd8e6
amberlex78 <amberlex78@gmail.com>
2022-08-03 20:42:49 +0300

    Add Frontend\HomeController, route (frontend.home), view

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['as' => 'frontend.'], static function () {
+    Route::get('/', HomeController::class)->name('home');
 });
